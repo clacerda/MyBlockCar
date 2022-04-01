d40a938a881a4a3333a2df0523d270565f395073
Claudio Vieira <36890797+clacerda@users.noreply.github.com>
2022-04-01 20:33:23 -0300

    Update myBlockCar.sol
    Include "marcha" and "aroRodas"

--- myBlockCar.sol
+++ myBlockCar.sol
@@ -6,18 +6,19 @@ contract myBlockCar {
 
     struct Carro{
         string marca;
-        string cor; 
-        uint8 rodas; 
+        string cor;
+        uint marcha;
+        uint8 aroRoda; 
     }
 
-    Carro[] carros;
+    Carro[] public carros;
 
-    function criarCarro(string memory _marca, string memory _cor, uint8 _rodas) external {
+    function criarCarro(string memory _marca, string memory _cor, uint8 marcha, uint8 _aroRoda) external {
         Carro memory novoCarro; 
 
         novoCarro.marca = _marca;
         novoCarro.cor = _cor;
-        novoCarro.rodas = _rodas;
+        novoCarro.aroRoda = _aroRoda;
 
         carros.push(novoCarro);
  
