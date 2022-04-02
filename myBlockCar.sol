aec39949d2562d1025d60328e3162f9dd16b0bea
Claudio Vieira <36890797+clacerda@users.noreply.github.com>
2022-04-02 12:28:09 -0300

    Update myBlockCar.sol
    Include change owner

--- myBlockCar.sol
+++ myBlockCar.sol
@@ -6,22 +6,37 @@ contract myBlockCar {
 
     struct Carro{
         string marca;
-        string cor;
-        uint marcha;
-        uint8 aroRoda; 
+        string cor; 
+        uint8 aroRoda;
+        bool ligar;
+        address owner;
     }
 
-    Carro[] public carros;
+    Carro[] carros;
 
-    function criarCarro(string memory _marca, string memory _cor, uint8 marcha, uint8 _aroRoda) external {
+    mapping(address => uint) private myCar;
+
+    function criarCarro(string memory _marca, string memory _cor, uint8 _aroRoda, bool _ligar) external {
         Carro memory novoCarro; 
 
         novoCarro.marca = _marca;
         novoCarro.cor = _cor;
         novoCarro.aroRoda = _aroRoda;
+        novoCarro.ligar = _ligar;
 
         carros.push(novoCarro);
- 
+    }
+
+
+    function changeOwnerCar(address _addr) external view returns (Carro memory) {
+        Carro memory novoCarro;
+        novoCarro.owner = _addr;
+    } 
+
+
+    function retornarCarro(uint8 identityCar) external view returns (Carro memory){
+       
+        return carros[identityCar];
     }
 
 }
