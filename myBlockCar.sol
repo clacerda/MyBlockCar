b317a82a75dd8f07c940f289a37aaef6390b001a
Claudio Vieira <36890797+clacerda@users.noreply.github.com>
2022-04-03 17:52:41 -0300

    Update myBlockCar.sol

--- myBlockCar.sol
+++ myBlockCar.sol
@@ -3,8 +3,7 @@
 pragma solidity ^0.8.0;
 
 contract myBlockCar {
-    //Owner cars
-     mapping(address => Carro) public carProperty;
+    
 
     //Struct of cars 
     struct Carro{
@@ -13,22 +12,30 @@ contract myBlockCar {
         uint aroRoda;
         bool ligar;
         uint256 idCar;
+        uint32 tanque;
         //address owner;
     }
+
+    //Owner cars
+    mapping(address => Carro) public carProperty;
+
     // create a list of cars
     Carro[] carros;
 
     //Function for creation of new car
-    function createCar(string memory _marca, string memory _cor, uint8 _aroRoda, bool _ligar) external {
+    function createCar(string memory _marca, string memory _cor, uint8 _aroRoda) external {
         Carro memory novoCarro; 
 
         novoCarro.marca = _marca;
         novoCarro.cor = _cor;
         novoCarro.aroRoda = _aroRoda;
-        novoCarro.ligar = _ligar;
+        novoCarro.ligar = false;
         novoCarro.idCar = carros.length;
         //novoCarro.owner = msg.sender;
+        novoCarro.tanque = 100;
         carros.push(novoCarro);
+
+        carProperty[msg.sender] = novoCarro;
     }
 
     //Returns a list of cars
@@ -36,12 +43,7 @@ contract myBlockCar {
         
         return carros;
     }
-
-    //Change a cars owner // function changeOwnerCar(address _addr, uint _id, address _to) external view returns (Carro memory) {
-    //      carProperty[_addr] =  
-    // }
-
-   
+ 
 
     //Returns a car
     function returnCar(uint _idCar) external view returns (Carro memory) {
@@ -51,9 +53,18 @@ contract myBlockCar {
 
  
     function ligarCarro(address _addr) external {
-        require(msg.sender == _addr);
-
+        //require(msg.sender == _addr, "Thief! You can not leave this car!");
+        require(carProperty[_addr].tanque < 10, "OMG, I think your fuel empity!");
         
+        carProperty[_addr].ligar = true;
+        carProperty[_addr].tanque - 10;
+    }
+
+    function desligarCarro(address _addr) external {
+        //require(msg.sender == _addr, "Thief! You can not leave this car!");
+        
+        carProperty[_addr].ligar = false;
+        carProperty[_addr].tanque - 1;
     }
 
   
